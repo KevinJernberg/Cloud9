4a93e09579c5089958af1930b4d1814120a1c298
KevinJernberg <a22kevje@student.his.se>
2024-04-09 19:39:15 +0200

    Kontrakt
    funka ej :cry:

---
+++ Assets/Scripts/Contract/ICloud.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICloud
+{
+    public struct SpawnTable
+    {
+        public GameObject Prefab;
+        public float spawnOdds;
+    }
+
+    List<SpawnTable> SpawnAble { get; set; }
+    int AmountSpawned { get; set; }
+    void Spawn(List<SpawnTable> spawnStats, out GameObject obj);
+}
+
+public class Cloud
+{
+    void function()
+    {
+        ICloud.SpawnTable gg = new ICloud.SpawnTable();
+        
+        //gg.spawnOdds / 
+    }
+}
