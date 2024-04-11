e27ef559dcac770eb54cf6a2acfd7726bf051ff6
LinnéaSkoldator <linnea@klangby.com>
2024-04-11 19:07:22 +0200

    man bryter inte nacken längre!

--- Assets/Scripts/Player/PlayerLook.cs
+++ Assets/Scripts/Player/PlayerLook.cs
@@ -6,8 +6,11 @@ using UnityEngine.InputSystem;
 
 public class PlayerLook : MonoBehaviour
 {
-    [SerializeField] private float _turnSpeed = 1;
+    [SerializeField] private float _turnSensitivity = 1;
+    [SerializeField] private float _rotationMin = -85f;
+    [SerializeField] private float _rotationMax = 85f;
     private float _turnDirection;
+    private float _cameraRotation;
     private Camera mainCamera;
 
     private void Awake()
@@ -18,7 +21,10 @@ public class PlayerLook : MonoBehaviour
     public void OnTurn(InputAction.CallbackContext context)
     {
         //The player should turn based on where the mouse is positioned
-        _turnDirection = context.ReadValue<float>();
-        mainCamera.transform.Rotate(_turnDirection * _turnSpeed/10, 0, 0);
+        _cameraRotation += context.ReadValue<float>() * _turnSensitivity / 10;
+        _cameraRotation = Mathf.Clamp(_cameraRotation, _rotationMin, _rotationMax);
+        mainCamera.transform.localEulerAngles = new Vector3(_cameraRotation, mainCamera.transform.localEulerAngles.y,
+            mainCamera.transform.localEulerAngles.z);
+        //mainCamera.transform.Rotate(_turnDirection * _turnSpeed/10, 0, 0);
     }
 }
