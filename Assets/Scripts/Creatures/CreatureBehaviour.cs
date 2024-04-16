aeaf0038316ea46e445c6280fb5fd0a6a0149672
WilLa03 <a22willa@student.his.se>
2024-04-16 16:15:33 +0200

    Creature materials and behaviour changes

--- Assets/Scripts/Creatures/CreatureBehaviour.cs
+++ Assets/Scripts/Creatures/CreatureBehaviour.cs
@@ -11,6 +11,7 @@ using UnityEngine;
 /// <typeparam name="TState"></typeparam>
 
 [RequireComponent(typeof(SphereCollider))]
+[RequireComponent(typeof(MeshRenderer))]
 public class CreatureBehaviour : MonoBehaviour
 {
     private CreatureState rootCreatureState = null;
@@ -21,26 +22,50 @@ public class CreatureBehaviour : MonoBehaviour
     private StateMachine<CreatureState> _stateMachine = null;
     private bool startTimer;
     private float timer;
+    private float despawntimer;
     [SerializeField] private float delayTillFlee;
-    [SerializeField] private SphereCollider detectionRadius;
+    [SerializeField] private float delayTillDespawn;
+    [SerializeField] private float speed;
+    private SphereCollider detectionRadius;
+    private MeshRenderer meshRenderer;
+    [SerializeField] private Material normal;
+    [SerializeField] private Material alert;
+    [SerializeField] private Material flee;
+    public LayerMask LookingFor;
+    private Transform enemy;
+    private Vector3 direction;
+    private bool setPos;
 
     public void Awake(){
+        detectionRadius = GetComponent<SphereCollider>();
+        detectionRadius.isTrigger = true;
+        meshRenderer = GetComponent<MeshRenderer>();
         rootCreatureState = new RootState(this, null);
         idleCreatureState = new IdleState(this, rootCreatureState);
         alertCreatureState = new AlertState(this, rootCreatureState);
         fleeCreatureState = new FleeState(this, rootCreatureState);
         _stateMachine = new StateMachine<CreatureState>();
         _stateMachine.InitializeMachine(idleCreatureState);
+        
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Range();
+        if (LookingFor == (LookingFor | (1 << other.transform.gameObject.layer)))
+        {
+            enemy= other.transform;
+            Range();
+        }
     }
+
+
 
     private void OnTriggerExit(Collider other)
     {
-        Range();
+        if (LookingFor == (LookingFor | (1 << other.transform.gameObject.layer)))
+        {
+            Range();
+        }
     }
 
     public void Range(){
@@ -56,6 +81,13 @@ public class CreatureBehaviour : MonoBehaviour
     {
         startTimer = false;
     }
+    
+    public void SetPos()
+    {
+        Vector3 enemyPos = enemy.position;
+        direction = new Vector3(transform.position.x -enemyPos.x, transform.position.y -enemyPos.y, transform.position.z -enemyPos.z).normalized;
+        setPos = true;
+    }
 
     private void Update()
     {
@@ -63,11 +95,21 @@ public class CreatureBehaviour : MonoBehaviour
         {
             timer += Time.deltaTime;
         }
-
         if (timer >= delayTillFlee)
         {
             Flee();
         }
+
+        if (setPos)
+        {
+            transform.position += direction * (speed * Time.deltaTime);
+            despawntimer+= Time.deltaTime;
+            if (despawntimer >= delayTillDespawn)
+            {
+                Destroy(gameObject);
+            }
+        }
+        
     }
 
     public void Flee(){
@@ -110,7 +152,12 @@ public class CreatureBehaviour : MonoBehaviour
     private class IdleState : CreatureState{
         
         public IdleState(CreatureBehaviour creature, CreatureState parent) : base(creature, parent){}
-        public override void Enter(){Debug.Log("IdleState: Enter()");{}}
+
+        public override void Enter()
+        {
+            Debug.Log("IdleState: Enter()");
+            Creature.meshRenderer.material = Creature.normal;
+        }
 
         public override void Range()
         {
@@ -125,6 +172,7 @@ public class CreatureBehaviour : MonoBehaviour
 
         public override void Enter(){
             Debug.Log("AlertState: Enter()");
+            Creature.meshRenderer.material = Creature.alert;
             Creature.StartTimer();
         }
 
@@ -147,7 +195,13 @@ public class CreatureBehaviour : MonoBehaviour
     private class FleeState : CreatureState{
         
         public FleeState(CreatureBehaviour creature, CreatureState parent) : base(creature, parent){}
-        public override void Enter(){Debug.Log("FleeState: Enter()");}
+
+        public override void Enter()
+        {
+            Debug.Log("FleeState: Enter()");
+            Creature.meshRenderer.material = Creature.flee;
+            Creature.SetPos();
+        }
         public override void Exit(){Debug.Log("FleeState: Exit()");}
     }
     #endregion
