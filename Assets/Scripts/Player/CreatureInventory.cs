427b2fd46ac5ae3ba9e4bd9044680a48c3287be0
LinnéaSkoldator <linnea@klangby.com>
2024-04-18 16:06:55 +0200

    comments

--- Assets/Scripts/Player/CreatureInventory.cs
+++ Assets/Scripts/Player/CreatureInventory.cs
@@ -1,14 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Rendering;
+/// <summary>
+/// Inventory for creatures - Linnéa
+/// </summary>
 public static class CreatureInventory
 {
     private static int _collectedCreatures;
+    private static int _maxInventorySpace;
 
-    public static void AddToInventory(int toAdd)
+    /// <summary>
+    /// Adds specific amount to inventory.
+    /// </summary>
+    /// <param name="toAdd">The amount to add</param>
+    /// <returns>Returns true if inventory is not full, and adds toAdd to the inventory. Returns false if inventory is full
+    /// and does not add to inventory.</returns>
+    public static bool AddToInventory(int toAdd)
     {
-        _collectedCreatures += toAdd;
-        Debug.Log($"Added to inventory. Inventory now contains: {_collectedCreatures}");
+        if(_collectedCreatures < _maxInventorySpace)
+        {
+            _collectedCreatures += toAdd;
+            Debug.Log($"Added to inventory. Inventory now contains: {_collectedCreatures}");
+            return true;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Sets the max amount of creatures in inventory, if the value is bigger than the current amount of creatures in inventory.
+    /// If overrideInventory is set to true it sets the inventory space to the value regardless if it is smaller than
+    /// the currently collected creatures amount.
+    /// </summary>
+    /// <param name="value">The value to set the inventory to</param>
+    /// <param name="overrideInventory">If true inventory limit is set to value regardless of how many creatures collected</param>
+    /// <returns>True if inventory max is changed, false otherwise</returns>
+    public static bool SetMaxInventorySpace(int value, bool overrideInventory = false)
+    {
+        if(overrideInventory)
+        {
+            _maxInventorySpace = value;
+            return true;
+        }
+        if (value > _collectedCreatures)
+        {
+            _maxInventorySpace = value;
+            return true;
+        }
+
+        return false;
     }
 }
--- Assets/Scripts/Player/SmallSuck.cs
+++ Assets/Scripts/Player/SmallSuck.cs
@@ -10,17 +10,17 @@ using UnityEngine.InputSystem;
 /// </summary>
 public class SmallSuck : MonoBehaviour
 {
+    [Tooltip("The max amount of creatures that the player can store.")]
     [SerializeField] private int maxAmountOfCreatures;
     [Tooltip("The force for the suck.")]
     [SerializeField] private float _suckForce = 10;
-
-    [SerializeField] private float collectRadius = 0.1f;
     private List<Rigidbody> _currentlySuckedCreatures;
     private bool _sucking;
 
     private void Awake()
     {
         _currentlySuckedCreatures = new List<Rigidbody>();
+        CreatureInventory.SetMaxInventorySpace(maxAmountOfCreatures);
     }
 
     private void Update()
@@ -35,6 +35,11 @@ public class SmallSuck : MonoBehaviour
         _sucking = context.performed;
     }
 
+    /// <summary>
+    /// Checks the list creatures in the colliderlist. If not 0 it adds a force on the creature towards the suck.
+    /// Then it calls a function to check if it can be sucked. Goes through the list backwards to make manipulation
+    /// of the list possible.
+    /// </summary>
     private void Suck()
     {
         for (int i = _currentlySuckedCreatures.Count - 1; i >= 0; i--)
@@ -47,7 +52,11 @@ public class SmallSuck : MonoBehaviour
         }
         
     }
-
+    /// <summary>
+    /// Checks if a specific creature is close enough to the suck to add to the inventory. Raycasts towards the
+    /// specific creature, if hit by the raycast it is close enough.
+    /// </summary>
+    /// <param name="creature">The rigidbody of the creature to check.</param>
     private void CheckIfSucked(Rigidbody creature)
     {
         Vector3 diff = Vector3.Normalize(creature.transform.position - transform.position);
@@ -72,26 +81,51 @@ public class SmallSuck : MonoBehaviour
         
         
     }
+    /// <summary>
+    /// If the inventory is not full it adds the creature to the inventory, removes it from the list and
+    /// destroys the GameObject.
+    /// </summary>
+    /// <param name="creature">The rigidbody of the creature to add.</param>
     private void AddToInventory(Rigidbody creature)
     {
-        CreatureInventory.AddToInventory(1);
-        _currentlySuckedCreatures.Remove(creature);
-        Destroy(creature.gameObject);
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        Vector3 diff = Vector3.Normalize(other.transform.position - transform.position);
-        float dot = Vector3.Dot(diff, transform.forward);
-        if(dot > 0.707)
+        if(CreatureInventory.AddToInventory(1))
         {
-            Debug.Log("in cone");
-            other.TryGetComponent(out Rigidbody otherRb);
-            if(otherRb != null) _currentlySuckedCreatures.Add(otherRb);
+            _currentlySuckedCreatures.Remove(creature);
+            Destroy(creature.gameObject);
         }
     }
-
+    /// <summary>
+    /// If the creature is inside the collider it checks if it is actually inside the cone, if yes, it adds
+    /// it to the list of creatures that can be sucked, otherwise it checks if it is in list of creature to suck
+    /// if so, it removes it.
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerStay(Collider other)
+    {
+        //This line can be removed when we fix collision layers and decide how to structure it.
+        if(!other.CompareTag("Creature"))return;
+        
+        Vector3 diff = Vector3.Normalize(other.transform.position - transform.position);
+        float dot = Vector3.Dot(diff, transform.forward);
+        other.TryGetComponent(out Rigidbody otherRb);
+        if(dot > 0.707 && !_currentlySuckedCreatures.Contains(otherRb))
+        {
+            Debug.Log("in cone");
+            
+            if(otherRb != null) _currentlySuckedCreatures.Add(otherRb);
+        }
+        else
+        {
+            if(_currentlySuckedCreatures != null && _currentlySuckedCreatures.Contains(otherRb))_currentlySuckedCreatures.Remove(otherRb);
+        }
+    }
+    /// <summary>
+    /// If the creature leaves the collision box it is removed from creatures to suck.
+    /// </summary>
+    /// <param name="other"></param>
     private void OnTriggerExit(Collider other)
     {
+        if(!other.gameObject.gameObject.CompareTag("Creature")) return;
         other.gameObject.TryGetComponent(out Rigidbody otherRb);
         if(otherRb == null)return;
         if (_currentlySuckedCreatures.Contains(otherRb)) _currentlySuckedCreatures.Remove(otherRb);
@@ -99,7 +133,7 @@ public class SmallSuck : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawSphere(transform.position, collectRadius);
+        Gizmos.DrawSphere(transform.position, 0.1f);
         //Debug.DrawRay(transform.position, diff * dot, Color.yellow, 10f);
         
     }
