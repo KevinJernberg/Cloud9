13c9bc78b73ad301038d2a6aaccf2b1e6268cae1
LinnéaSkoldator <linnea@klangby.com>
2024-04-18 16:45:43 +0200

    no movement if map open

--- Assets/Scripts/InteractObjects/MapPedistal.cs
+++ Assets/Scripts/InteractObjects/MapPedistal.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MapPedistal : MonoBehaviour, IInteract
 {
     [SerializeField] private GameObject mapUI;
+    [SerializeField] private PlayerInput _playerInputComponent;
     public void Interact()
     {
         ToggleMap();
@@ -15,6 +17,20 @@ public class MapPedistal : MonoBehaviour, IInteract
         //TODO: Limit the player ability to move and look around;
         Cursor.lockState = CursorLockMode.Confined;
         mapUI.SetActive(!mapUI.activeSelf);
+        _playerInputComponent.SwitchCurrentActionMap(mapUI.activeSelf ? "UI" : "Player");
+    }
+
+    public void OnCloseMap(InputAction.CallbackContext context)
+    {
+        if(mapUI.activeSelf)
+        {
+            if (context.started)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                mapUI.SetActive(false);
+                _playerInputComponent.SwitchCurrentActionMap("Player");
+            }
+        }
     }
     
 }
