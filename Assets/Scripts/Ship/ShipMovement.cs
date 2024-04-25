8252d83565d329fa9583c04ddc1e57b12679f410
WilLa03 <a22willa@student.his.se>
2024-04-25 18:37:07 +0200

    ship tweaks

--- Assets/Scripts/Ship/ShipMovement.cs
+++ Assets/Scripts/Ship/ShipMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 /// <summary>
-/// Controls the ships movement. - Linnéa
+/// Controls the ships movement. - Linnéa, William
 /// </summary>
 public class ShipMovement : MonoBehaviour
 {
@@ -16,6 +16,9 @@ public class ShipMovement : MonoBehaviour
     [Tooltip("How fast you accelerate.")] [SerializeField] private float _accelerationSpeed = 1;
     [Tooltip("How fast you can turn around.")] [SerializeField] private float _turnSensitivity = 1;
     [Tooltip("The maximum speed the rigidbody can have when fleeing")][SerializeField] private float maxSpeed;
+    
+    [Header("Temporary")]
+    [Tooltip("If True the ship will only be able to turn when it is driven forward")][SerializeField] private bool onlyTurnWhenDrive;
 
     private void Awake()
     {
@@ -26,9 +29,9 @@ public class ShipMovement : MonoBehaviour
     {
         if (_moving)
         {
-            _rb.AddForce(transform.TransformDirection(_direction) * (_accelerationSpeed), ForceMode.Acceleration);
-            //_rb.AddForce(transform.TransformDirection(_direction) * (_accelerationSpeed * Time.deltaTime), ForceMode.Acceleration);
             transform.Rotate(0, _turnDirection * _turnSensitivity* Time.deltaTime, 0);
+            _rb.AddForce(transform.TransformDirection(_direction) * (_accelerationSpeed), ForceMode.Acceleration);
+            
             if (_rb.velocity.magnitude > maxSpeed)
                 _rb.velocity = Vector3.ClampMagnitude(_rb.velocity, maxSpeed);
         }
@@ -39,7 +42,30 @@ public class ShipMovement : MonoBehaviour
     {
         var temp = context.ReadValue<Vector3>();
         _direction = new Vector3(0, 0, temp.z);
-        _turnDirection = temp.x;
+        if (onlyTurnWhenDrive)
+        {
+            if (temp.z == 0f)
+            {
+                _turnDirection = 0f;
+            }
+            else
+            {
+                _turnDirection = temp.x;
+            }
+        }
+        else
+        {
+            _turnDirection = temp.x;
+        }
+        
+        
         _moving = context.performed;
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(transform.position, transform.TransformDirection(_direction)*100);
+        
+
     }
 }
