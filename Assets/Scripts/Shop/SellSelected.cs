b3740aa027c4ec50f2d502b86a094c75403eacc2
WilLa03 <a22willa@student.his.se>
2024-05-02 16:32:44 +0200

    Hotbar begin fix

--- Assets/Scripts/Shop/SellSelected.cs
+++ Assets/Scripts/Shop/SellSelected.cs
@@ -2,30 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-
+/// <summary>
+/// System for selecting a slot in the hotbar and then selling it - William
+/// </summary>
 public class SellSelected : MonoBehaviour
 {
     [SerializeField] private EventSystem system;
 
-    private GameObject selected;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private int selected;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
-    public void Select()
+    public void Select(int slot)
     {
-        selected = system.currentSelectedGameObject;
+        selected = slot;
     }
     public void Sell()
     {
-        Debug.Log($"Sell {selected.name}");
+        Debug.Log($"Selling for {Inventory.itemSlots[selected].item.sellingPrice}");
+        //Debug.Log($"Sell {selected.name}");
     }
 }
