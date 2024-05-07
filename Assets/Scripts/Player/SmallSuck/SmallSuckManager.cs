e79f2d2191ca41a4392a6ec656a5c84994e9ac80
LinnéaSkoldator <linnea@klangby.com>
2024-05-07 15:55:32 +0200

    input actions

--- Assets/Scripts/Player/SmallSuck/SmallSuckManager.cs
+++ Assets/Scripts/Player/SmallSuck/SmallSuckManager.cs
@@ -8,9 +8,9 @@ public class SmallSuckManager : MonoBehaviour
     //När de blir sucked ska man kalla på creatureBehaviour beingSucked
     //Det kommer finnas 3 olika colliders att hålla koll på, och de kommer att ge olika resultat
     // Start is called before the first frame update
-    private SmallSuckArea _smallSuckArea;
-    private MiddleSuckArea _middleSuckArea;
-    private BigSuckArea _bigSuckArea;
+    [SerializeField]private SuckArea _smallSuckArea;
+    [SerializeField]private SuckArea _middleSuckArea;
+    [SerializeField]private SuckArea _bigSuckArea;
 
     ///Frågor:
     /// Vad händer om man slutar mitt i? Ska man kunna göra det? ja, escape
@@ -20,13 +20,6 @@ public class SmallSuckManager : MonoBehaviour
     /// prioritera den zon som ger mest poäng
     /// när man går in i minigamet ska small suck flyttas till mitten av skärmen och man ska
     /// se lite mer inzoomat på den.
-    private void Awake()
-    {
-        _smallSuckArea = GetComponentInChildren<SmallSuckArea>();
-        _middleSuckArea = GetComponentInChildren<MiddleSuckArea>();
-        _bigSuckArea = GetComponentInChildren<BigSuckArea>();
-    }
-
     private void SuckMiniGame()
     {
         
