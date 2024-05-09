bb51735b8d84610d234383d9757269feb61751da
KevinJernberg <a22kevje@student.his.se>
2024-05-09 15:48:21 +0200

    Timings for ship storage deposit

--- Assets/Scripts/Ship/ShipStorage/ShipStorage.cs
+++ Assets/Scripts/Ship/ShipStorage/ShipStorage.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class ShipStorage : MonoBehaviour, IInteract
 {
     [SerializeField] private PlayerInput _playerInputComponent;
 
+    [SerializeField] private Slider storageCapacitySlider;
+    
     [SerializeField] private ItemData[] storage = new ItemData[10];
+    
 
-    [SerializeField, Tooltip("Should be the same length as the animation to deposit materials")] private float waitTimeUntilDone;
+    [Header("Timings")]
+    [SerializeField, Tooltip("The time it takes to start depositing, in seconds")] private float waitTimeUntilStart;
+    [SerializeField, Tooltip("The time it take to deposit 1 item, in second")] private float waitTimeBetweenDeposits;
+    [SerializeField, Tooltip("The time it takes until the deposit is done")] private float waitTimeAfter;
 
     [Header("Audio")] 
     public UIAudio uIAudio;
@@ -43,6 +51,9 @@ public class ShipStorage : MonoBehaviour, IInteract
             }
         }
 
+        yield return new WaitForSeconds(waitTimeUntilStart);
+        storageCapacitySlider.value = (float)(storage.Length-spaceLeft) / storage.Length;
+
         int amountAdded = 0;
         for (int i = 0; i < Inventory.itemSlots.Count; i++)
         {
@@ -50,19 +61,20 @@ public class ShipStorage : MonoBehaviour, IInteract
             {
                 spaceLeft = CheckStorageAmountLeft();
                 Debug.Log(Inventory.itemSlots[i].itemCount);
+                if (amountAdded >= amountToAdd)
+                    break;
                 if (spaceLeft <= 0)
                     break;
 
-                yield return new WaitForSeconds(waitTimeUntilDone / amountToAdd);
+                yield return new WaitForSeconds(waitTimeBetweenDeposits);
                 Inventory.ChangeItemAmount(-1, (Inventory.itemSlots[i].item));
                 storage[storage.Length - spaceLeft] = Inventory.itemSlots[i].item;
+                Debug.Log($"{CheckStorageAmountLeft()}  {storage.Length}");
                 amountAdded++;
-                if (amountAdded >= amountToAdd)
-                {
-                    break;
-                }
+                storageCapacitySlider.value = (float)(storage.Length-spaceLeft+1) / storage.Length;
             }
         }
+        yield return new WaitForSeconds(waitTimeAfter);
         
         _playerInputComponent.SwitchCurrentActionMap("Player");
         Debug.Log("Deposit Complete");
