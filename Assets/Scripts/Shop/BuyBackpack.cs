52311ec6065af90d191c3e50daeec2b853ddfe15
WilLa03 <a22willa@student.his.se>
2024-05-14 18:02:31 +0200

    comments

--- Assets/Scripts/Shop/BuyBackpack.cs
+++ Assets/Scripts/Shop/BuyBackpack.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+/// <summary>
+/// Holds the event for when you try to buy a backpack upgrade - William
+/// </summary>
 public class BuyBackpack : MonoBehaviour
 {
     public static UnityAction OnBuy;
--- Assets/Scripts/Shop/BuyShipSpace.cs
+++ Assets/Scripts/Shop/BuyShipSpace.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-
+/// <summary>
+/// Holds the event for when you try to buy a shipspace upgrade - William
+/// </summary>
 public class BuyShipSpace : MonoBehaviour
 {
     public static UnityAction OnBuy;
