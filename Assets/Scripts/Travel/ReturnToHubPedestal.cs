624564c98b245767b8e916c14ba579c1c5aa6630
KevinJernberg <a22kevje@student.his.se>
2024-05-16 15:48:12 +0200

    ReturnToHubPedestal not annoying

--- Assets/Scripts/Travel/ReturnToHubPedestal.cs
+++ Assets/Scripts/Travel/ReturnToHubPedestal.cs
@@ -1,7 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ReturnToHubPedestal : MonoBehaviour
 {
+    [SerializeField] private GameObject mapUI;
+    [SerializeField] private PlayerInput _playerInputComponent;
+    
+    [Header("Audio")] 
+    public UIAudio uIAudio;
+    
+    public void Interact()
+    {
+        ToggleMap();
+    }
+
+    private void ToggleMap()
+    {
+        //TODO: Limit the player ability to move and look around;
+        Cursor.lockState = CursorLockMode.Confined;
+        mapUI.SetActive(!mapUI.activeSelf);
+        _playerInputComponent.SwitchCurrentActionMap(mapUI.activeSelf ? "UI" : "Player");
+        uIAudio.MapToggleAudio(transform);
+    }
+
+    public void OnCloseMap(InputAction.CallbackContext context)
+    {
+        if(mapUI.activeSelf)
+        {
+            if (context.started)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                mapUI.SetActive(false);
+                _playerInputComponent.SwitchCurrentActionMap("Player");
+                uIAudio.MapCloseAudio(transform);
+            }
+        }
+    }
+    
+    public 
 }
